1a3524a221bb17b5dc0a1c7910589e2fd9753e6a
Shivraj <shivraj@geekyants.com>
2017-09-21 18:18:10 +0530

    RN fixes

--- src/boot/setup.tsx
+++ src/boot/setup.tsx
@@ -1,4 +1,3 @@
-import * as Expo from "expo";
 import * as React from "react";
 import { StyleProvider } from "native-base";
 import { Provider } from "react-redux";
@@ -9,9 +8,8 @@ import getTheme from "../theme/components";
 import variables from "../theme/variables/platform";
 export interface Props {}
 export interface State {
-	store: Object,
-	isLoading: boolean,
-	isReady: boolean,
+	store: Object;
+	isLoading: boolean;
 }
 export default class Setup extends React.Component<Props, State> {
 	constructor() {
@@ -19,26 +17,10 @@ export default class Setup extends React.Component<Props, State> {
 		this.state = {
 			isLoading: false,
 			store: configureStore(() => this.setState({ isLoading: false })),
-			isReady: false,
 		};
-	}
-	componentWillMount() {
-		this.loadFonts();
-	}
-	async loadFonts() {
-		await Expo.Font.loadAsync({
-			Roboto: require("native-base/Fonts/Roboto.ttf"),
-			Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-			Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-		});
-
-		this.setState({ isReady: true });
 	}
 
 	render() {
-		if (!this.state.isReady || this.state.isLoading) {
-			return <Expo.AppLoading />;
-		}
 		return (
 			<StyleProvider style={getTheme(variables)}>
 				<Provider store={this.state.store}>
